902ff950d8cd7ae57e64589775f97cc7f0c18d44
phatniii <phattanisonk65@nu.ac.th>
2025-01-27 16:52:43 +0700

    cm

--- lib/provider/transactionProvider.dart
+++ lib/provider/transactionProvider.dart
@@ -3,11 +3,11 @@ import 'package:flutter/foundation.dart';
 
 class TransactionProvider with ChangeNotifier {
    List<Transaction> transactions = [
-    Transaction(title: 'Book', amount: 1000, datetime: DateTime(2025, 1, 20)), 
-    Transaction(title: 'Shirt', amount: 850, datetime: DateTime(2025, 1, 15)), 
-    Transaction(title: 'Skirt', amount: 600, datetime: DateTime(2025, 1, 10)),
-    Transaction(title: 'Shoes', amount: 2500, datetime: DateTime(2025, 1, 5)),
-    Transaction(title: 'Cardigan', amount: 1200, datetime: DateTime(2025, 1, 1)),
+    Transaction(title: 'Book', amount: 1000, datetime: DateTime.now()), 
+    Transaction(title: 'Shirt', amount: 850, datetime: DateTime.now()), 
+    Transaction(title: 'Skirt', amount: 600, datetime: DateTime.now()),
+    Transaction(title: 'Shoes', amount: 2500, datetime: DateTime.now()),
+    Transaction(title: 'Cardigan', amount: 1200, datetime: DateTime.now()),
   ];
   List<Transaction> getTransaction(){
     return transactions;
